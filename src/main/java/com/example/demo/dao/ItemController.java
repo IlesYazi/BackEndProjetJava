0894642b758ecdc7762ecb59a67be1ce4373201c
IlesYazi <96123733+IlesYazi@users.noreply.github.com>
2023-04-17 20:42:48 +0200

    v2

--- src/main/java/com/example/demo/dao/ItemController.java
+++ src/main/java/com/example/demo/dao/ItemController.java
@@ -11,18 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entities.Etudiant;
+import com.example.demo.entities.Prof;
 import com.example.demo.entities.Voiture;
 
 @RestController
 public class ItemController {
-	
-	@Autowired
-	private VoitureRepository vr;
-	
-	@RequestMapping(path = "/voiture", method = RequestMethod.GET)
-	public List<Voiture> getVoiture() {
-		return vr.findAll();
-	}
 	
 	/*
 	 * @RequestMapping(path = "/voiture", params= {"name","color"} ,method =
@@ -31,6 +25,22 @@ public class ItemController {
 	 * 
 	 * return vr.findAll(name); }
 	 */
+	
+	@Autowired
+	private VoitureRepository vr;
+	
+	@Autowired
+	private ProfRepository prof;
+	
+	@Autowired
+	private EtudiantRepository etud;
+	
+	/* -------------- VOITURE -------------- */
+	
+	@RequestMapping(path = "/voiture", method = RequestMethod.GET)
+	public List<Voiture> getVoiture() {
+		return vr.findAll();
+	}
 	
 	@RequestMapping(path = "/voiture", method = RequestMethod.POST)
 	public String addVoiture(@RequestBody Voiture voiture) {
@@ -55,4 +65,65 @@ public class ItemController {
 		return "Voiture modifier";
 	}
 	
+	/* -------------- PROFESSEUR -------------- */
+	
+	@RequestMapping(path = "/professeur", method = RequestMethod.GET)
+	public List<Prof> getProfesseur() {
+		return prof.findAll();
+	}
+	
+	@RequestMapping(path = "/professeur", method = RequestMethod.POST)
+	public String addProfesseur(@RequestBody Prof professeur) {
+		prof.save(professeur);
+		return professeur.getFirstName();
+	}
+	
+	@RequestMapping(path = "/professeur/{id}", method = RequestMethod.DELETE)
+	public String deleteIdProfesseur(@PathVariable Long id) {
+		prof.deleteById(id);
+		return "Professeur supprimer";
+	}
+	
+	@RequestMapping(path = "/professeur/{id}", method = RequestMethod.PUT)
+	public String majProfesseur(@PathVariable Long id, @RequestBody Prof professeur) {
+		@SuppressWarnings("deprecation")
+		Prof p = prof.getById(id);
+		p.setFirstName(professeur.getFirstName());
+		p.setLastName(professeur.getLastName());
+		p.setSpecialite(professeur.getSpecialite());
+		prof.save(p);
+		return "Professeur modifier";
+	}
+	
+	/* -------------- ETUDIANT -------------- */
+	
+	@RequestMapping(path = "/etudiant", method = RequestMethod.GET)
+	public List<Etudiant> getEtudiant() {
+		return etud.findAll();
+	}
+	
+	@RequestMapping(path = "/etudiant", method = RequestMethod.POST)
+	public String addEtudiant(@RequestBody Etudiant etudiant) {
+		etud.save(etudiant);
+		return etudiant.getFirstName();
+	}
+	
+	@RequestMapping(path = "/etudiant/{id}", method = RequestMethod.DELETE)
+	public String deleteIdEtudiant(@PathVariable Long id) {
+		etud.deleteById(id);
+		return "Etudiant supprimer";
+	}
+	
+	@RequestMapping(path = "/etudiant/{id}", method = RequestMethod.PUT)
+	public String majEtudiant(@PathVariable Long id, @RequestBody Etudiant etudiant) {
+		@SuppressWarnings("deprecation")
+		Etudiant e = etud.getById(id);
+		e.setFirstName(etudiant.getFirstName());
+		e.setLastName(etudiant.getLastName());
+		e.setAge(etudiant.getAge());
+		e.setSexe(etudiant.getSexe());
+		e.setIdProf(etudiant.getIdProf());
+		etud.save(e);
+		return "Etudiant modifier";
+	}
 }
